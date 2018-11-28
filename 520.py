f11ed41749d2bc9f7983f932431ac0ddc6661365
jsxcool <jsx2017@outlook.com>
2018-11-28 11:33:53 -0500

    .

--- 520.py
+++ 520.py
@@ -2,6 +2,7 @@ from pandas import Series, DataFrame
 import pandas as pd
 import numpy as np
 from numpy import nan as NA
+import matplotlib.pyplot as plt
 
 '''
 x = [90,61,24,53,94,5]
@@ -17,7 +18,19 @@ df = DataFrame(data)
 print(df.corr())
 '''
 
-data = pd.read_csv('/home/jiang/Desktop/nyc-rolling-sales.csv')
+data = pd.read_csv('nyc-rolling-sales.csv')
 data = data.drop([data.columns[0], 'APARTMENT NUMBER'], axis=1)
-print(data.head())
-print(data.corr())
+#print(data.head())
+#print(data.corr())
+correlation = data.corr()
+fig, ax = plt.subplots(figsize=(20, 20))
+ax.matshow(correlation)
+plt.xticks(range(len(correlation.columns)), correlation.columns)
+plt.yticks(range(len(correlation.columns)), correlation.columns)
+plt.show()
+
+
+
+'''
+
+'''
